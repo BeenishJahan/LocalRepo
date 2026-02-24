69be6c694831a1e897e6abdc6720c42e220c3b30
Beenish Jahan <beenishjahan2003@gmail.com>
2026-02-24 23:19:30 +0530

    checking

--- file.py
+++ file.py
@@ -1,8 +1,2 @@
-import math
-a = 100
-b = math.sqrt(a)
-print(f"square root of {a} is {b}")
-
-r = 2500
-s = math.sqrt(r)
-print(f"square root of {r} is {s}")
+name = 'Beenish Jahan'
+print(f"Hello {name}")
